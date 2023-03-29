060e70b6a8e29fd3d2c5b24e163bce0863b0a6fe
Melvin5808 <127872703+Melvin5808@users.noreply.github.com>
2023-03-29 11:06:11 -0400

    Created the hello function with docstring
    Prints hello

--- hello_goodbye.py
+++ hello_goodbye.py
@@ -3,3 +3,11 @@ Driver: Melvin Vasquez-Gomez
 Navigator: Lorenzo Abernathy
 March 29, 2023
 """
+
+def hello():
+    """ Prints hello
+    Side Effects: prints hello
+    Attributes: there are no attributes
+    """
+
+    print("hello")
